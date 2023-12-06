233875f37c55ee7245e3cca630703e18f3370b1e
Yftach Herzog <yherzog@redhat.com>
2023-12-06 14:07:27 +0200

    chore(RHTAPWATCH-636): add compose protocols
    Create protocols and basic structure for generating generic and odcs
    composes.
    
    Signed-off-by: Yftach Herzog <yherzog@redhat.com>

---
+++ compose_generator/odcs_configurations_generator.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+from .protocols import ComposeConfigurations, ComposeConfigurationsGenerator
+
+
+@dataclass(frozen=True)
+class ODCSConfigurations(ComposeConfigurations):
+    """
+    Configurations to be used for requesting an ODCS compose
+    """
+
+
+@dataclass(frozen=True)
+class ODCSConfigurationsGenerator(ComposeConfigurationsGenerator):
+    """
+    Generate odcs configurations based on container and content_sets yamls.
+
+    :param container_data: data loaded from container.yaml
+    :param content_sets_data: data loaded from content_sets.yaml
+    """
+    container_data: dict
+    content_sets_data: dict
+
+    def __call__(self) -> ODCSConfigurations:
+        raise NotImplementedError()
