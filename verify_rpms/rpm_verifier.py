cc44c7e61da2243247e2bb8c79b306f18b5cb7f6
Yftach Herzog <yherzog@redhat.com>
2023-12-14 14:29:49 +0200

    chore(RHTAPWATCH-680): generate rpm_verifier output
    Generate the script output and decide whether it should fail or not
    
    Signed-off-by: Yftach Herzog <yherzog@redhat.com>

--- verify_rpms/rpm_verifier.py
+++ verify_rpms/rpm_verifier.py
@@ -2,6 +2,7 @@
 
 """Verify RPMs are signed"""
 
+import sys
 import tempfile
 from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
@@ -19,6 +20,9 @@ class ProcessedImage:
     image: str
     unsigned_rpms: list[str]
 
+    def __str__(self) -> str:
+        return f"image: {self. image} | unsigned RPMs: {', '.join(self.unsigned_rpms)}"
+
 
 def get_rpmdb(container_image: str, target_dir: Path, runner: Callable = run) -> Path:
     """Extract RPM DB from a given container image reference"""
@@ -31,14 +35,24 @@ def get_unsigned_rpms(rpmdb: Path, runner: Callable = run) -> list[str]:
 
 
 def generate_output(
-    unsigned_rpms: Iterable[ProcessedImage], fail_unsigned: bool
-) -> None:
+    processed_images: Iterable[ProcessedImage], fail_unsigned: bool
+) -> tuple[bool, str]:
     """
-    Given a mapping between images and unsigned RPMs, print out the images
-    having unsigned RPMs, together with their unsigned RPMs.
-    Exit with error in case unsigned RPMs found and the relevant flag provided.
+    Generate the output that should be printed to the user based on the processed images
+    and decide whether the script should fail or not
+
+    :param processed_images: each element contains a name and a list of unsigned images
+    :param fail_unsigned: whether the script should fail in case unsigned rpms found
     """
-    raise NotImplementedError()
+    with_unsigned_rpms = [img for img in processed_images if img.unsigned_rpms]
+    if not with_unsigned_rpms:
+        output = "No unsigned RPMs found."
+        fail = False
+    else:
+        output = "\n".join([str(img) for img in with_unsigned_rpms])
+        output = f"Found unsigned RPMs:\n{output}"
+        fail = fail_unsigned
+    return fail, output
 
 
 def parse_image_input(image_input: str) -> list[str]:
@@ -103,7 +117,10 @@ def main(img_input: str, fail_unsigned: bool, workdir: Path) -> None:
             processor, container_images
         )
 
-    generate_output(list(processed_images), fail_unsigned)
+    fail, output = generate_output(list(processed_images), fail_unsigned)
+    if fail:
+        sys.exit(output)
+    print(output)
 
 
 if __name__ == "__main__":
